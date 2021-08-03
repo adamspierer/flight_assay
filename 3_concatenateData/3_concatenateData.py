ef9e9ee51ceb0ff79e63f50e0bbcf11a5a46b4e0
Adam Spierer <anspierer@gmail.com>
2021-08-03 16:42:47 -0400

    New output file for all_flies.csv, reordered code, and added instructions

--- 3_concatenateData/3_concatenateData.py
+++ 3_concatenateData/3_concatenateData.py
@@ -1,3 +1,31 @@
+## Script for processing flight data
+## Written August 3, 2021 by Adam Spierer
+## 
+## The purpose of this script is to take the result tables created by 
+##    FIJI/ImageJ macro for analyzing flight performance data and bringing
+##    the result files into a single file with all spots (all_flies.csv)
+##    and an aggregated results file with the mean, standard deviation,
+##    and counts for each combination in the `experimental_details` 
+##    variable (results.csv).
+##
+## Areas of this script to modify by experiment:
+##  1. The user can input the directory with the csv files for each analyzed 
+##    image as either a command line argument, or as a hardcoded input [line 33/34]. 
+##    If the user enters a command line argument, then the files created by this 
+##    program will save to the same directory. If hardcoded, the user can change 
+##    the saveDirectory path.
+##  2. The user should go into FIJI/ImageJ to figure out the conversion factor
+##    for pixels per meters and modify the corresponding variable [line 37].
+##  3. The user should change the `experiment_details` [line 40] to represent
+##    the column names corresponding with the underscore-separated fields in
+##    the image file name. The number of items in this list should match those 
+##    in the image file name.
+##
+## To run this script:
+##    python <path_to_script> <optional_path_to_csv_directory>
+##
+##
+##
 ########################################################################
 ## User specified variables ############################################
 ########################################################################
@@ -12,7 +40,7 @@ pixels_per_meter = 784
 experiment_details=['condition','genotype','sex','replicate']
 
 ########################################################################
-## Python functions and script #########################################
+## Python functions ####################################################
 ########################################################################
 
 ## Load libraries
@@ -43,17 +71,22 @@ def create_df(csvDirectory, File):
   _d['Y'] = 1 - _d.Y
   return(_d)
 
+########################################################################
+## Python script #######################################################
+########################################################################
+
 ## Check directory is valid
 try:
   if os.path.isdir(argv[1]):
     csvDirectory = argv[1]
+    saveDirectory = argv[1]
     print('Reading files from directory:',csvDirectory)
+    print('Saving result.csv file to:   ',saveDirectory)
 except:
-  print('No command line arguments entered.\nReading files from hard coded directory:',csvDirectory)
-
+  print('No command line arguments entered.\nReading/saving files from/to hard coded directory:',csvDirectory)
 
 ## Concatenate DataFrames
-d = pd.concat([create_df(csvDirectory,item) for item in os.listdir(csvDirectory) if item.endswith('csv')])
+d = pd.concat([create_df(csvDirectory,item) for item in os.listdir(csvDirectory) if (item.endswith('csv') and item != 'all_flies.csv' and item != 'results.csv')])
 
 ## Groupby functions
 ##   Mean
@@ -70,6 +103,10 @@ g = g.rename({'Y':'count'}, axis = 1).reset_index()
 h = pd.merge(e,f)
 h = pd.merge(h,g).drop(['index'],axis=1)
 
-## Save results
+## Save file with aggregated results
 h.to_csv(saveDirectory + "results.csv")
+
+## Save file with all flies
+d.to_csv(saveDirectory + "all_flies.csv")
+
 print('END')
